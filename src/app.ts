2aa71b19f3299e88ef6fed6dd7ffcf53b3cf3f75
HongJunHyeok <edb1631@naver.com>
2021-10-21 23:46:47 +0900

    TEST: DOM rendering using array template

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
-window.addEventListener("DOMContentLoaded", () => {
-  document.write("DOM Rendered");
-});
+const container = document.getElementById("root");
+
+let headerComponent: string[] = [];
+
+headerComponent.push("<ul>");
+
+[1, 2, 3, 4].forEach((item) => headerComponent.push(`<li>${item}</li>`));
+
+headerComponent.push("</ul>");
+
+container.innerHTML = headerComponent.join("");
