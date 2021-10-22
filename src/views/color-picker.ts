ad72280fd67be6dd139a076807c8d9f1ef613564
HongJunHyeok <edb1631@naver.com>
2021-10-23 01:33:23 +0900

    REFACTOR: color picker class extends color view class

--- src/views/color-picker.ts
+++ src/views/color-picker.ts
@@ -1,26 +1,10 @@
 import template from "./color-picker.template";
+import CoreView from "./core-view";
 
-class ColorPicker {
-  protected _template: string;
-  protected _container: string;
-
+class ColorPicker extends CoreView {
   constructor(container: string) {
-    this._container = container;
-    this._template = template;
+    super(container, template);
   }
-
-  render = (appendChild: boolean = false) => {
-    //* appendChild속성은 container요소의 자식으로써 렌더링 할것인지 덮어쓰기 할 것인지에 대한 옵션
-    const container = document.querySelector(this._container);
-
-    if (container && appendChild) {
-      const divFragment = document.createElement("div");
-      divFragment.innerHTML = this._template;
-      container.appendChild(divFragment.children[0]);
-    } else {
-      container.innerHTML = this._template;
-    }
-  };
 }
 
 export default ColorPicker;
